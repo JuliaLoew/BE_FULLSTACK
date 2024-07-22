6ea5418bebcb9829d76340aa89f9a9b64575e06c
Henry Nguyen <henrylinh94@yahoo.de>
2024-07-22 17:47:28 +0200

    GET posts and GET postsById

--- get.js
+++ get.js
@@ -1,1 +1,11 @@
-export default get;
+async function getAllPosts(req, res) {
+    try {
+        const result = await Client.query('SELECT * FROM posts');
+        res.status(200).json(result.rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Error fetching posts'});
+    }
+}
+
+export default getAllPosts;
--- getid.js
+++ getid.js
@@ -1,1 +1,15 @@
-export default getid;
+async function getPostsbyId (req, res) {
+    const {id} = req.params;
+    try {
+        const result = await client.query('SELECT * FROM posts WHERE id = $1', [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({error: 'Post not found'});
+        }
+        res.json(result.rows[0]);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+}
+
+export default getPostsbyId;
