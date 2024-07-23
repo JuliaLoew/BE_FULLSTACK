0e58105e84122dd6fa5ca6912f681489fd033a93
Julia Löw <julia.loew@gmx.net>
2024-07-23 17:57:38 +0200

    Korrektur

--- post.js
+++ post.js
@@ -4,20 +4,17 @@ const { Client } = pg;
 async function post(req, res) {
     const time= new Date();
     const timestamp=`${time.getFullYear()}-${time.getMonth()}-${time.getDate()} // ${time.getHours()}:${time.getMinutes()}`;
+   
     const client = new Client({
-            user: 'ahmed',
-            password: '12345',
-            host: 'localhost',
-            port: 5432,
-            database: 'test'
-    });
+      connectionString: process.env.PG_URI });
+
     await client.connect();
-    const { id,author,title, content,cover,date } = req.body;
+    const {title, content,cover,data,author } = req.body;
 
   try {
     const result = await client.query(
-      'INSERT INTO posts (id,author,title,content,cover,date) VALUES ($1, $2,$3,$4,$5,$6) RETURNING *',
-        [id,author,title,content,cover,date] 
+      'INSERT INTO fullstack (id,title,content,cover,data,author) VALUES ($1, $2,$3,$4,$5,$6) RETURNING *',
+        [id,title,content,cover,data,author] 
     );
     res.status(201).json(result.rows[0]);
   } catch (err) {
